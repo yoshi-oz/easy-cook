af4da55fe973d5726aa4b08edb426ff48305184f
yoshi-oz <hagimenoippo3150@gmail.com>
2020-04-29 18:44:13 +0900

    fixed__new-post--view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    # @posts = @post.post.includes(:user)
   end
 
   def create
@@ -11,6 +10,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:post).permit(:content, :image, :name).merge(user_id: current_user.id)
   end
 end
