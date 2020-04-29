fed59f9106af4eeab426fe38f79eab2f41519f37
yoshi-oz <hagimenoippo3150@gmail.com>
2020-04-29 11:36:00 +0900

    Adjustment-front-view

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -8,4 +8,9 @@ class FormsController < ApplicationController
     Post.create(post_params)
   end
   
+  private
+  def post_params
+    params.require(:post).permit(:content, :image).merge(user_id: current_user.id)
+  end
+  
 end
