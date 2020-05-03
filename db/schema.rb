fe3687feb39fd4d9b95f93406dfc4db714eb7f8f
yoshi-oz <hagimenoippo3150@gmail.com>
2020-05-03 19:15:20 +0900

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20200503092357) do
 
-  create_table "movies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.string   "title"
-    t.string   "movie"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_movies_on_user_id", using: :btree
-  end
-
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "content"
@@ -46,6 +36,5 @@ ActiveRecord::Schema.define(version: 20200503092357) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "movies", "users"
   add_foreign_key "posts", "users"
 end
