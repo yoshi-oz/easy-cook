510a5f21072f84e0e1004862a72e7417a4f10a82
yoshi-oz <hagimenoippo3150@gmail.com>
2020-05-03 18:26:36 +0900

    add-movie-model

---
+++ db/migrate/20200503092357_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.string :title
+      t.string :movie
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200428085752) do
+ActiveRecord::Schema.define(version: 20200503092357) do
+
+  create_table "movies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "movie"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -36,5 +46,6 @@ ActiveRecord::Schema.define(version: 20200428085752) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "movies", "users"
   add_foreign_key "posts", "users"
 end
